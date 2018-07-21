fb483c74de6ee821025fbf760e28930b41d1ea17
Kevin Huang <khuang@mz.com>
2018-07-20 19:48:37 -0700

    update

--- contracts/scam/scam.cpp
+++ contracts/scam/scam.cpp
@@ -59,3 +59,23 @@ public:
         auto owner_transactions = transactions.get_index<N(byowner)>();
     }
 };
+
+#define EOSIO_ABI_EX( TYPE, MEMBERS ) \
+extern "C" { \
+   void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+      if( action == N(onerror)) { \
+         /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s "active permission */ \
+         eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
+      } \
+      auto self = receiver; \
+      if( code == self || code == N(eosio.token) || action == N(onerror) ) { \
+         TYPE thiscontract( self ); \
+         switch( action ) { \
+            EOSIO_API( TYPE, MEMBERS ) \
+         } \
+         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
+      } \
+   } \
+}
+
+EOSIO_ABI_EX(transaction, (createpet))
