f2aac4018ba3dc61d58bb729ac927e12b4056175
Kevin Huang <khuang@mz.com>
2018-07-20 20:14:49 -0700

    update

--- contracts/scam/scam.cpp
+++ contracts/scam/scam.cpp
@@ -13,52 +13,28 @@ using namespace eosio;
 using std::string;
 using std::hash;
 
-class scam : public eosio::contract {
+void scam::createTransaction(name owner, string pet_name) {
 
-public:
-    struct st_transactions {
-        //uuid id;
-        uint32_t id;
-        name owner;
-        string name;
-        uint32_t created_at;
-        uint32_t ammount;
+    require_auth(owner);
 
-        uint64_t primary_key() const { return id; }
+    //uuid new_id = _next_id();
 
-        uint64_t get_transactions_by_owner() const { return owner.value; }
-    };
+    transactions.emplace(owner, [&](auto &r) {
+        st_transactions transaction{};
+        transaction.id = 1;
+        transaction.name = "baby";
+        transaction.owner = owner;
+        transaction.created_at = now();
+        transaction.ammount = 0;
 
+        //pet.type = (hash_str(pet_name) + pet.created_at + pet.id + owner) % pc.last_pet_type_id;
 
-    typedef multi_index<N(transactions), st_transactions,
-            indexed_by<N(byowner), const_mem_fun<st_transactions, uint64_t, &st_transactions::get_transactions_by_owner>>
-    > _tb_transactions;
-
-    _tb_transactions transactions;
-
-    void createTransaction(name owner, string pet_name) {
-
-        require_auth(owner);
-
-        //uuid new_id = _next_id();
-
-        transactions.emplace(owner, [&](auto &r) {
-            st_transactions transaction{};
-            transaction.id = 1;
-            transaction.name = "baby";
-            transaction.owner = owner;
-            transaction.created_at = now();
-            transaction.ammount = 0;
-
-            //pet.type = (hash_str(pet_name) + pet.created_at + pet.id + owner) % pc.last_pet_type_id;
-
-            r = transaction;
-        });
+        r = transaction;
+    });
 
 
-        auto owner_transactions = transactions.get_index<N(byowner)>();
-    }
-};
+    auto owner_transactions = transactions.get_index<N(byowner)>();
+}
 
 #define EOSIO_ABI_EX( TYPE, MEMBERS ) \
 extern "C" { \
--- contracts/scam/scam.hpp
+++ contracts/scam/scam.hpp
@@ -4,5 +4,34 @@
 
 #ifndef PROJECT_SCAM_H
 #define PROJECT_SCAM_H
-
 #endif //PROJECT_SCAM_H
+
+
+class scam : public eosio::contract {
+  public:
+    scam(account_name self)
+            :contract(self),
+            transactions(_self,_self){};
+
+    struct st_transactions {
+        //uuid id;
+        uint32_t id;
+        name owner;
+        string name;
+        uint32_t created_at;
+        uint32_t ammount;
+
+        uint64_t primary_key() const { return id; }
+
+        uint64_t get_transactions_by_owner() const { return owner.value; }
+        //EOSLIB_SERIALIZE( game, (challenger)(host)(turn)(winner)(board))
+    };
+
+    typedef multi_index<N(transactions), st_transactions,
+            indexed_by<N(byowner), const_mem_fun<st_transactions, uint64_t, &st_transactions::get_transactions_by_owner>>
+    > _tb_transactions;
+
+    _tb_transactions transactions;
+
+    void createTransaction(name owner, string pet_name);
+};
