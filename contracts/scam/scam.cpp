bd6cb7d9d69fc252e188f12ad94b67da51d3f5ea
Kevin Huang <khuang@mz.com>
2018-07-22 02:42:36 -0700

    fix

--- contracts/scam/scam.cpp
+++ contracts/scam/scam.cpp
@@ -40,9 +40,9 @@ void scam::createpool(account_name owner, string poolname) {
     require_auth(owner);
 
     //uuid new_id = _next_id();
-    //auto owner_pools = pools.get_index<N(byowner)>();
-    //auto pitr = owner_pools.get(N(blockfishbgp));
-    //pools.erase( pitr );
+    auto owner_pools = pools.get_index<N(byowner)>();
+    auto pitr = owner_pools.get(name{owner});
+    pools.erase( pitr );
 
     print( "=============createpool:, ", name{owner} );
     pools.emplace(owner, [&](auto &r) {
