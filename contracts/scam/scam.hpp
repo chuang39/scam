15fe5a9b060bbee1d14bd024aac5d8717eb733c4
Kevin Huang <khuang@mz.com>
2018-07-28 12:44:04 -0700

    fix

--- contracts/scam/scam.hpp
+++ contracts/scam/scam.hpp
@@ -23,7 +23,7 @@ class scam : public eosio::contract {
 public:
     scam(account_name self):contract(self),accounts(_self, _self){};
 
-    // @abi table st_accounts i64
+    // @abi table accounts i64
     struct st_accounts {
         uint64_t id;
         string name;
