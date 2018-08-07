922e5a60df9a622ee056e46fe7e04de82f5dd26f
Kevin Huang <khuang@mz.com>
2018-08-07 00:46:57 -0700

    Revert "remove all"
    This reverts commit 47a6151919f9446abaa63c3bab1a1dca3498376d.

--- contracts/scam/scam.cpp
+++ contracts/scam/scam.cpp
@@ -4,3 +4,161 @@
 
 #include "scam.hpp"
 
+void scam::createacnt(string name, string city, uint32_t zipcode,
+                      uint8_t rating, uint8_t type, string logo,
+                      string picture, string website, string phone) {
+    accounts.emplace(_self, [&](auto& account){
+            account.id = accounts.available_primary_key();
+            account.name = name;
+            account.city = city;
+            account.rating = 0;
+            account.type = type;
+            account.zipcode = zipcode;
+            account.logo = logo;
+            account.picture = picture;
+            account.website = website;
+            account.phone = phone;
+            account.num_revs = 0;
+        });
+}
+
+void scam::createrevw(string user, string business, uint32_t rating, string line) {
+    reviews.emplace(_self, [&](auto& review){
+            review.id = reviews.available_primary_key();
+            review.user = user;
+            review.business = business;
+            review.rating = rating;
+            review.line = line;
+            review.created_at = now();
+        });
+    for( const auto& account : accounts ) {
+        if (account.name == business) {
+            accounts.modify(account, 0, [&](auto &r) {
+                r.rating = ((r.num_revs * r.rating) + rating) / (r.num_revs + 1);
+                r.num_revs += 1;
+            });
+        }
+    }
+}
+void scam::deleterevw() {
+    //for( const auto& account : accounts ) {
+    //    accounts.erase(account);
+    //}
+    //for( const auto& review : reviews ) {
+    //    reviews.erase(review);
+    //}
+
+    auto ite = accounts.begin();
+    while(ite != accounts.end()) {
+        ite = accounts.erase(ite);
+    }
+    //auto ite = reviews.begin();
+    //while(ite != reviews.end()) {
+    //    ite = reviews.erase(ite);
+    //}
+
+}
+void scam::reserve(string user, string business, string time) {
+}
+EOSIO_ABI( scam, (createacnt)(createrevw)(reserve)(deleterevw))
+
+/*
+void scam::createtran(const account_name from, const asset& quantity) {
+
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must deposit positive quantity" );
+    //eosio_assert(code == N(eosio.token), "I reject your non-eosio.token deposit");
+    eosio_assert(quantity.symbol == string_to_symbol(4, "EOS"),
+                 "only accepts EOS for deposits");
+
+    // get pool. Check if the pool is expired or not.
+    auto owner_pools = pools.get_index<N(byowner)>();
+    auto pitr = owner_pools.get(N(blockfishbgp));
+    eosio_assert(pitr != owner_pools.end(), "E404|Invalid pool");
+    eosio_assert(pitr->status == 1, "E404|The scam has ended.");
+
+    auto aitr = accounts.find(from);
+    if(aitr == accounts.end()) {
+        aitr = accounts.emplace(_self, [&](auto& account){
+            account.owner = name{from};
+            account.created_at = now();
+        });
+        print(" ~~~~~~~~~owner:", aitr->owner, ", ammount: ",
+              aitr->ammount, ",  created_at:", aitr->created_at, "\n");
+    }
+
+    action(
+            permission_level{ from, N(active) },
+            N(eosio.token), N(transfer),
+            std::make_tuple(from, _self, quantity, std::string(""))
+    ).send();
+
+    // add key to user account
+    accounts.modify(aitr, 0, [&](auto &r) {
+        r.key_balance += ceil(quantity.amount / 0.5);
+    });
+
+    // update pool
+    pitr->eos
+
+    //print(" ~~ID=", pitr.id, ", owner:", pitr.owner, ", ammount: ",
+    //      pitr.ammount, ", end_at:",
+    //      pitr.end_at, ", created_at:", pitr.created_at, "\n");
+
+
+    // update dividend for account
+
+    //auto owner_pools = pools.get_index<N(byowner)>();
+    //auto pitr = owner_pools.get(N(blockfishbgp));
+    //print(" ~~ID=", pitr.id, ", owner:", pitr.owner, ", ammount: ",
+    //      pitr.ammount, ", end_at:",
+    //      pitr.end_at, ", created_at:", pitr.created_at, "\n");
+
+
+}
+
+void scam::createpool(account_name owner, string poolname) {
+
+    require_auth(_self);
+
+    auto owner_pools = pools.get_index<N(byowner)>();
+    auto pitr = owner_pools.find(name{owner});
+    owner_pools.erase( pitr );
+    print( "Create pool by owner=%s ", name{owner} );
+
+    pools.emplace(owner, [&](auto &r) {
+        st_pools pool{};
+        pool.id = 0;
+        //transaction.name = "baby";
+        pool.owner = name{owner};
+        pool.created_at = now();
+        pool.end_at = now() + 24 * 3600;
+        pool.ammount = 10;
+        r = pool;
+    });
+
+    for( const auto& pool : pools ) {
+        print(" ID=", pool.id, ", owner:", pool.owner, ", ammount: ",
+              pool.ammount, ", end_at:",
+              pool.end_at, ", created_at:", pool.created_at, "\n");
+    }
+    owner_pools = pools.get_index<N(byowner)>();
+    for( const auto& pool : owner_pools ) {
+        print(" ~~ID=", pool.id, ", owner:", pool.owner, ", ammount: ",
+              pool.ammount, ", end_at:",
+              pool.end_at, ", created_at:", pool.created_at, "\n");
+    }
+
+}
+
+void scam::getpool(account_name owner) {
+    print("Items sorted by primary key:\n");
+    for( const auto& pool : pools ) {
+        print(" ID=", pool.id, ", owner:", pool.owner, ", ammount: ",
+              pool.ammount, ", end_at:",
+              pool.end_at, ", created_at:", pool.created_at, "\n");
+    }
+}
+
+EOSIO_ABI( scam, (createtran)(createpool)(getpool)(ping))
+*/
--- contracts/scam/scam.hpp
+++ contracts/scam/scam.hpp
@@ -20,5 +20,177 @@ using std::hash;
 
 
 class scam : public eosio::contract {
+public:
 
+    /*
+    scam(account_name self):contract(self)
+            ,accounts(_self, _self)
+            ,reviews(_self, _self){};
+
+    // @abi table accounts i64
+    struct st_accounts {
+        uint64_t id;
+        string name;
+        string city;
+        uint32_t zipcode;
+        uint8_t rating;
+        uint8_t type;
+
+        uint64_t primary_key() const { return id; }
+
+        EOSLIB_SERIALIZE(st_accounts, (id)(name)(city)(zipcode)(rating)(type))
+    };
+
+    typedef multi_index<N(accounts), st_accounts> _tb_accounts;
+    _tb_accounts accounts;
+
+    // @abi table reviews i64
+    struct st_reviews {
+        uint64_t id;
+        string user;
+        string business;
+        uint8_t rating;
+        string line;
+
+        uint64_t primary_key() const { return id; }
+
+        EOSLIB_SERIALIZE(st_reviews, (id)(user)(business)(rating)(line))
+    };
+
+    typedef multi_index<N(reviews), st_reviews> _tb_reviews;
+    _tb_reviews reviews;
+
+    void createacnt(string name, string city, uint32_t zipcode, uint8_t rating, uint8_t type);
+    void createrevw(string user, string business, uint32_t rating, string line);
+    void deleterevw();
+*/
+    /*
+    scam(account_name self):contract(self){};
+    void ping();
+     */
+
+    scam(account_name self):contract(self)
+            ,accounts(_self, _self)
+            ,reviews(_self, _self){};
+
+    // @abi table accounts i64
+    struct st_accounts {
+        uint64_t id;
+        string name;
+        string city;
+        uint32_t zipcode;
+        uint8_t rating;
+        uint8_t type;
+        string logo;
+        string picture;
+        string website;
+        uint8_t num_revs;
+        string phone;
+
+        uint64_t primary_key() const { return id; }
+
+        EOSLIB_SERIALIZE(st_accounts, (id)(name)(city)(zipcode)(rating)(type)(logo)(picture)(website)(num_revs)(phone))
+    };
+
+    typedef multi_index<N(accounts), st_accounts> _tb_accounts;
+    _tb_accounts accounts;
+
+    // @abi table reviews i64
+    struct st_reviews {
+        uint64_t id;
+        string user;
+        string business;
+        uint8_t rating;
+        string line;
+        uint32_t created_at;
+
+        uint64_t primary_key() const { return id; }
+
+        EOSLIB_SERIALIZE(st_reviews, (id)(user)(business)(rating)(line)(created_at))
+    };
+
+    typedef multi_index<N(reviews), st_reviews> _tb_reviews;
+    _tb_reviews reviews;
+
+    void createacnt(string name, string city, uint32_t zipcode, uint8_t rating,
+                    uint8_t type, string logo, string picture, string website, string phone);
+    void createrevw(string user, string business, uint32_t rating, string line);
+    void deleterevw();
+    void reserve(string user, string business, string time);
+/*
+  public:
+    scam(account_name self)
+            :contract(self),
+            transactions(_self,_self),
+            pools(_self, _self),
+            accounts(_self, _self){};
+
+    struct st_scam_config {
+        asset    creation_fee = asset{0,S(4,EOS)};
+    };
+
+    struct st_accounts {
+        name owner;
+        uint32_t created_at;
+        uint32_t key_balance;
+        asset eos_balance;
+
+        uint64_t primary_key() const { return owner; }
+
+        uint64_t get_transactions_by_owner() const { return owner.value; }
+        EOSLIB_SERIALIZE(st_accounts, (owner)(created_at)(key_balance)(eos_balance))
+    };
+
+    struct st_transactions {
+        //uuid id;
+        uint32_t id;
+        name owner;
+        //string name;
+        uint32_t created_at;
+        uint32_t ammount;
+
+        uint64_t primary_key() const { return id; }
+
+        uint64_t get_transactions_by_owner() const { return owner.value; }
+        //EOSLIB_SERIALIZE( game, (challenger)(host)(turn)(winner)(board))
+    };
+
+    struct st_pools {
+        //uuid id;
+        uint32_t id;
+        // TODO: use name
+        //string name;
+        name owner;
+        uint8_t status; // 0 for inactive; 1 for active
+        uint32_t created_at;
+        uint32_t end_at;
+        uint32_t key_balance;
+        asset eos_balance;
+
+        uint64_t primary_key() const { return id; }
+
+        uint64_t get_pools_by_owner() const { return owner; }
+        EOSLIB_SERIALIZE(st_pools, (id)(owner)(status)(created_at)(end_at)(key_balance)(eos_balance))
+    };
+
+    typedef multi_index<N(pools), st_pools,
+            indexed_by<N(byowner), const_mem_fun<st_pools, uint64_t, &st_pools::get_pools_by_owner>>
+    > _tb_pools;
+
+    typedef multi_index<N(transactions), st_transactions,
+            indexed_by<N(byowner), const_mem_fun<st_transactions, uint64_t, &st_transactions::get_transactions_by_owner>>
+    > _tb_transactions;
+
+    typedef multi_index<N(accounts), st_accounts> _tb_accounts;
+
+    _tb_transactions transactions;
+    _tb_pools pools;
+    _tb_accounts accounts;
+
+    void createtran(const account_name from, const asset& quantity);
+
+    void createpool(account_name owner, string poolname);
+    void getpool(account_name owner);
+    void ping(account_name receiver);
+*/
 };
