102445b9c1c3102150faefcd9257d0ddf42cd73a
Kevin Huang <khuang@mz.com>
2018-08-10 17:21:59 -0700

    asdf

--- contracts/scam/scam.cpp
+++ contracts/scam/scam.cpp
@@ -17,7 +17,8 @@ void scam::createpool(const name owner, const string poolname) {
         r.created_at = now();
         r.end_at = now() + 24 * 3600;
         r.key_balance = 10;
-        r.eos_balance = asset(1220);
+        //r.eos_balance = asset(0);
+        //r.key_price = asset(1220);
     });
 }
 
