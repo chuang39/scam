3a2501098b558c1fceea69391090a8c6b499633b
Kevin Huang <khuang@mz.com>
2018-08-12 00:31:50 -0700

    ad

--- contracts/scam/scam.cpp
+++ contracts/scam/scam.cpp
@@ -131,4 +131,21 @@ void scam::apply(account_name contract, account_name act) {
     switch (act) { EOSIO_API(scam, (withdraw)(createpool)(deleteall)(reset)); };
 }
 
+#define EOSIO_ABI_EX( TYPE, MEMBERS ) \
+extern "C" { \
+   void apply(uint64_t receiver, uint64_t code, uint64_t action) { \
+      auto self = receiver; \
+      if( action == N(onerror)) { \
+         /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s "active permission */ \
+         eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
+      } \
+      if(code == self || code == N(eosio.token) || action == N(onerror)) { \
+         TYPE thiscontract(self); \
+         switch(action) { \
+            EOSIO_API(TYPE, MEMBERS) \
+         } \
+      } \
+   } \
+}
+
 EOSIO_ABI_EX(scam, (withdraw)(createpool)(deleteall)(reset))
--- contracts/scam/scam.hpp
+++ contracts/scam/scam.hpp
@@ -20,25 +20,6 @@ using std::string;
 using std::hash;
 
 
-#define EOSIO_ABI_EX( TYPE, MEMBERS ) \
-extern "C" { \
-   void apply(uint64_t receiver, uint64_t code, uint64_t action) { \
-      auto self = receiver; \
-      if( action == N(onerror)) { \
-         /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s "active permission */ \
-         eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
-      } \
-      if(code == self || code == N(eosio.token) || action == N(onerror)) { \
-         TYPE thiscontract(self); \
-         switch(action) { \
-            EOSIO_API(TYPE, MEMBERS) \
-         } \
-      } \
-   } \
-}
-
-
-
 class scam : public eosio::contract {
   public:
     scam(account_name self)
