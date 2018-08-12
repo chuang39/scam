91e8d7ab8eb4cb49e6332f04d39b185351502913
Kevin Huang <khuang@mz.com>
2018-08-11 23:20:40 -0700

    aa

--- contracts/scam/scam.cpp
+++ contracts/scam/scam.cpp
@@ -60,25 +60,6 @@ void scam::deposit(const currency::transfer &t, account_name code) {
 //@abi action
 void withdraw(const account_name to, const asset& quantity) {
 
-    require_auth(to);
-
-    // find user
-    auto itr = accounts.find(account);
-    eosio_assert(itr != _balance.end(), "user does not exist");
-
-    // set quantity
-    eosio_assert(quantity.amount + itr->balance > quantity.amount,
-                 "integer overflow adding withdraw balance");
-    quantity.amount += itr->balance;
-
-    // clear balance
-    _balance.modify(itr, _this_contract, [&](auto &p) { p.balance = 0; });
-
-    // withdraw
-    action(permission_level{_this_contract, N(active)}, N(eosio.token),
-           N(transfer), std::make_tuple(_this_contract, account, quantity,
-                                        std::string("from eosday.io")))
-            .send();
 
 /*
 
